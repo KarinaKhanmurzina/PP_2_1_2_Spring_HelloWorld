ffc2e20549beee5ab76e0b46bcdfd0012279f9bf
Karina <khanmurzinakarina17@gmail.com>
2022-03-04 16:02:37 +0300

    решение задачи: создание бина, изменение его области видимости на "prototype"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,17 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean == beanTwo);
+
+        ApplicationContext applicationContextCat =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat beanCat = (Cat) applicationContextCat.getBean("cat");
+        System.out.println(beanCat.getName());
+        Cat beanCatTwo = (Cat) applicationContextCat.getBean("cat");
+        System.out.println(beanCat == beanCatTwo);
+
     }
 }
